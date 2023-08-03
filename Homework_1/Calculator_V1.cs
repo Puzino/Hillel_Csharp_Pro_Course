26382d1fefb05d644e4b90ff818e0a23994f8b89
Puzino <puzino000@gmail.com>
2023-08-03 14:54:04 +0300

    Add files homework 1

---
+++ Homework_1/Calculator_V1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Hillel_Csharp_Pro
+{
+	public class Calculator_V1
+	{
+		private double first_number;
+		private double second_number;
+
+        public Calculator_V1(double first_number, double second_number)
+		{
+			this.first_number = first_number;
+			this.second_number = second_number;
+		}
+
+		public void Addition()
+		{
+			Console.WriteLine($"{first_number + second_number}");
+		}
+
+		public void Dividing()
+		{
+			if (first_number == 0 || second_number == 0)
+			{
+				Console.WriteLine("На 0 делить нельзя!");
+			}
+			else
+			{
+                Console.WriteLine($"{first_number / second_number}");
+            }
+        }
+
+		public void Deduction()
+		{
+            Console.WriteLine($"{first_number - second_number}");
+
+        }
+
+		public void Multiplication()
+		{
+            Console.WriteLine($"{first_number * second_number}");
+        }
+    }
+}
+
---
+++ Homework_1/README.md
@@ -0,0 +1,1 @@
+# Hillel_Csharp_Pro
