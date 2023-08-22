f1acc402b61fa72c1c3f666ffddce0bde9fe0d8d
Puzino <puzino000@gmail.com>
2023-08-22 22:06:59 +0300

    Homework 5

---
+++ Homework_5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Homework_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Task №1
+        using Piece piece = new("Gamlet", "Shekspir", "Tragedy", "1603");
+
+        //Task №2
+        using Shop shop = new Shop("Analnaya Radost`", "Gagarina 23", "Sex Shop");
+
+        // Task №3
+        piece.ShowInfo();
+
+        Console.WriteLine();
+
+        shop.ShowInfo();
+
+    }
+}
+
---
+++ Homework_5/Shop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Homework_5
+{
+	public class Shop : IDisposable
+    {
+        private bool disposed = false;
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Type { get; set; }
+
+        public Shop(string name, string address, string type)
+		{
+			Name = name;
+			Address = address;
+			Type = type;
+		}
+
+		public void ShowInfo()
+		{
+			Console.WriteLine($"Name: {Name}\nAddress: {Address}\nType: {Type}");
+		}
+
+        public void Dispose()
+        {
+            Console.WriteLine($"Shop {Name} has been deleted");
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed) return;
+            if (disposing)
+            {
+                // Очищяем ресурсы
+            }
+            disposed = true;
+        }
+
+        ~Shop()
+		{
+            Console.WriteLine($"Shop {Name} has been deleted");
+        }
+    }
+}
+
