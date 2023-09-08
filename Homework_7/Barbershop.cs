838c8f6a0b09a97b959e3614525dd06bdbb86271
Puzino <puzino000@gmail.com>
2023-09-08 16:51:54 +0300

    Done Homework 7

---
+++ Homework_7/Barbershop.cs
@@ -0,0 +1,34 @@
+﻿namespace Homework_7;
+class Barbershop
+{
+    private readonly object lockObject = new object();
+    private bool barberSleeping = true;
+
+    public void VisitBarbershop(string visitorName)
+    {
+        lock (lockObject)
+        {
+            if (barberSleeping)
+            {
+                Console.WriteLine($"{visitorName} розбудив перукаря і чекає на стрижку.");
+                barberSleeping = false;
+                // Розпочніть стрижку
+            }
+            else
+            {
+                Console.WriteLine($"{visitorName} чекає в приймальні.");
+                // Додайте відвідувача до черги
+            }
+        }
+    }
+
+    public void LeaveBarbershop(string visitorName)
+    {
+        lock (lockObject)
+        {
+            Console.WriteLine($"{visitorName} завершив стрижку і покидає перукарню.");
+            barberSleeping = true;
+            // Повідомте про завершену стрижку та розбудьте перукаря, якщо в черзі є інші відвідувачі
+        }
+    }
+}
---
+++ Homework_7/Visitor.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework_7;
+class Visitor
+{
+    private readonly Barbershop barbershop;
+    private readonly string name;
+
+    public Visitor(Barbershop barbershop, string name)
+    {
+        this.barbershop = barbershop;
+        this.name = name;
+    }
+
+    public void EnterBarbershop()
+    {
+        barbershop.VisitBarbershop(name);
+        // Реалізуйте логіку для стрижки тут
+        barbershop.LeaveBarbershop(name);
+    }
+}
